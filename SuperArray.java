0397ac47329484e9b540f6a49d9a0cc2e7511c80
Tina Nguyen <thetinalenguyen@gmail.com>
2020-10-31 21:17:42 -0400

    added methods add and get

--- SuperArray.java
+++ SuperArray.java
@@ -8,7 +8,27 @@ public class SuperArray {
     size = 0;
   }
 
-  
+  public int size() {
+//returns the number of elements in this list
 
+    return size;
+
+  }
+
+  public boolean add(String element){
+//appends the specified element to the end of this list
+
+    data[size] = element;
+    size++;
+    return true;
+
+  }
+
+  public String get(int index){
+//returns the elemt at the specified position in this list
+
+    return data[index];
+
+  }
 
 }
