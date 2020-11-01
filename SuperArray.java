85412955b127b962d784ebab40f021d55bb80207
Tina Nguyen <thetinalenguyen@gmail.com>
2020-10-31 21:00:24 -0400

    instance variables and constructor added

---
+++ SuperArray.java
@@ -0,0 +1,14 @@
+public class SuperArray {
+
+  private String [] data;
+  private int size; //The current size
+
+  public SuperArray() {
+    data = new String[10];
+    size = 0;
+  }
+
+  
+
+
+}
