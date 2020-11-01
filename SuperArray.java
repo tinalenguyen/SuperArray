a502b44db411eea1939c2fd0720921257de75e8c
Tina Nguyen <thetinalenguyen@gmail.com>
2020-10-31 21:39:36 -0400

    Added add and set

--- SuperArray.java
+++ SuperArray.java
@@ -31,4 +31,27 @@ public class SuperArray {
 
   }
 
+  public String set(int index, String element){
+//replaces the element at the specified position in this list with the specified element, return the value you replaced
+
+    data[index] = element;
+    return element;
+
+  }
+
+  private void resize(){
+//increase the capacity: create a larger array, copy values from the original array to new one, assign new one to instance variable
+
+
+  }
+
+  public boolean add(String element){
+//appends the specified element to the end of this list
+    String[] newArray = new String[size + 10]
+    for (int i = 0; i < data.length; i++){
+      newArray[i] = data[i];
+    }
+    newArray = data;
+  }
+
 }
